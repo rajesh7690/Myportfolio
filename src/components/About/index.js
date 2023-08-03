ac68ad2a084ba582d54df9ec3397c8f0ecaad134
Rajesh <rajeshsahu7683995090@gmail.com>
2023-08-03 13:20:04 +0530

    email js update

---
+++ src/components/About/index.js
@@ -0,0 +1,50 @@
+import './index.scss';
+import { useEffect,useState } from 'react';
+import AnimatedLetters from '../AnimatedLetters'
+import Loader from 'react-loaders'
+
+const About = () =>{
+    const [letterClass, setLetterClass] = useState('text-animate')
+    useEffect(() => {
+         setTimeout(() => {
+            setLetterClass('text-animate-hover')
+        }, 3000)
+    }, [])
+
+    return (
+        <>
+        <div className='container about-page'>
+            <div className='text-zone'>
+                <h1>
+                    <AnimatedLetters
+                        letterClass={letterClass}
+                        strArray={['A','b','o','u','t',' ','m','e' ]}
+                        idx={15}
+                    />
+                </h1>
+                <p>
+                    I'm very ambitious front-end developer looking for a role in
+                    established IT company with the opportunity to work with the latest
+                    technologies on challenging and diverse projects.
+                </p>
+                <p align="LEFT">
+                    I'm quietly confident, naturally curious, and perpetually working on
+                    improving my chops one design problem at a time.
+                </p>
+                <p>
+                    If I need to define myself in one sentence that would be a creative
+                    person, always open to learn new technology, a sports fanatic,
+                    photography and videography enthusiast, youtuber and tech-obsessed!!!
+                </p>
+
+            </div>
+            <div>
+                <span class="content"></span>
+            </div>
+        </div>
+        <Loader type="pacman"/>
+        </>
+    )
+}
+
+export default About;
